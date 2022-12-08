3988df3f392519576f7858dc9a0a9249fc6e1908
Stepenko Ruslan <117574556+papapyc@users.noreply.github.com>
2022-12-08 12:09:14 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,32 @@
+// Завдання 1
+
+let city = "Kyiv";
+
+city = "Lviv";
+
+let address = city;
+
+alert(address);
+
+// Завдання 2
+
+let n = 29;
+
+let result = Math.pow(n, 3);
+
+console.log(result);
+
+// Завдання 3
+
+let pen = 4;
+
+let pensil = 6;
+
+let p_pen = 5.25;
+
+let p_pensil = 3.5;
+
+let total = pen * p_pen + pensil * p_pensil;
+
+document.write("Загальна вартість :", total);
+
